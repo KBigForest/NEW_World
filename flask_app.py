267777af7d8f6910d1a3f311b846f1a6e1f71858
KBigForest <gksfka12@naver.com>
2023-04-30 12:56:50 +0900

    필요없는 experience삭제  및 flask_app 변동

--- flask_app.py
+++ flask_app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/experience/')
-def page1():
-    return render_template('experience.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
